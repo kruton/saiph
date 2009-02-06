b28d2f166a8364dd88cb7128387cbb13caa6f184
Kenny Root <kenny@the-b.org>
2009-02-06 05:10:38 -0600

    Add back ignore for Elbereth XON branch

--- Analyzers/Elbereth.cpp
+++ Analyzers/Elbereth.cpp
@@ -98,7 +98,9 @@ bool Elbereth::request(const Request &request) {
 			if (((burned || digged) && elbereth_count > 0) || ((dusted || frosted) && elbereth_count >= 3)) {
 				/* we should rest */
 				command = "20" SEARCH;
+#if 1 /* FLOW_CONTROL */
 				ignore_next_prompts = 2;
+#endif
 				priority = request.priority;
 				return true;
 			} else if (!burned && !digged && elbereth_count < 3) {
